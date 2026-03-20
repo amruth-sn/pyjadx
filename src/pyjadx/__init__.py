eed07530dde9561e8f8431a9eba81bb5420a0570
Amruth <amruth@bu.edu>
2026-03-19 22:30:17 -0400

    update version to 0.1.1

--- src/pyjadx/__init__.py
+++ src/pyjadx/__init__.py
@@ -14,7 +14,7 @@ from pyjadx._types import JavaClass, JavaField, JavaMethod, JavaPackage
 
 from pyjadx._jars import JADX_VERSION
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__: list[str] = [
     "__version__",
